98cd2050365d9bbfa83e9213cd2c071d2464558a
Bence Pimpli <pimplibence@gmail.com>
2019-02-04 21:16:16 +0100

    Fixup tslint.json
    Add Example code

--- src/reducers/counter.reducer.ts
+++ src/reducers/counter.reducer.ts
@@ -1,9 +1,29 @@
 import { Action } from 'redux';
+import { CounterActions } from '../actions/counter.actions';
 
 const initialState = {
     counter: 0
 };
 
 export const counterReducer = (state: any = initialState, action: Action<any>) => {
+
+    switch (action.type) {
+        case CounterActions.INCREMENT: {
+            return {
+                ...state,
+                counter: ++state.counter
+            };
+        }
+        case CounterActions.DECREMENT: {
+            return {
+                ...state,
+                counter: --state.counter
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+
     return state;
 };
